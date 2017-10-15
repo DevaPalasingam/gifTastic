33080afa8ee0ebbc1f2cdeda49e6b4ae80ab0071
Deva <deva.palasingam@gmail.com>
2017-10-15 10:15:01 -0500

    still working on makeGif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,8 +59,9 @@ function makeButtons() {
 	$ (".animal").on("click", function() {
 
 		console.log(this);
+		var buttonClicked = $ (this).attr("data-name");
 
-		makeGif(this);
+		makeGif(buttonClicked);
 
 	});
 	//Click Button:============================================
@@ -70,20 +71,46 @@ function makeButtons() {
 
 //makeGif:
 function makeGif(gifButton) {
+	console.log("button pressed: " + gifButton);
+
 	$.ajax({
-		url: "https://api.giphy.com/v1/gifs/search?api_key=zMbakXvDgLzdHgGLD8ljNumQmXZ2ThJU&q=" +gifButton + "&limit=10&offset=0&rating=PG&lang=en",
+		url: "https://api.giphy.com/v1/gifs/search?api_key=zMbakXvDgLzdHgGLD8ljNumQmXZ2ThJU&q=" + gifButton + "&limit=10&offset=0&rating=PG&lang=en",
 		method: "GET"
 	})
 	.done(function(gifInfo) {
 		
+		
+		//gifLoop: this loop grabs gif info from giphy, makes an img tag with the info, then prints it on the page
 		for(var i = 0; i < gifInfo.data.length; i++) {
+			
+			console.log(gifInfo);
+			
+			//this code grabs the data from giphy
 			var imageRating = gifInfo.data[i].rating;
 			console.log("image rating: " + imageRating);
-			var stillUrl = gifInfo.data[i].images.fixed_height_small_still.url;
+			var stillUrl = gifInfo.data[i].images.fixed_height_still.url;
 			console.log("still image url: " + stillUrl);
-			var animateUrl = gifInfo.data[i].images.fixed_height_small.url;
+			var animateUrl = gifInfo.data[i].images.fixed_height.url;
 			console.log("animated url: " + animateUrl);
+			
+
+			//this code makes an image tag and gives it the data from giphy
+			var imageTag = $("<img>");
+			imageTag.attr("src", stillUrl);
+			imageTag.attr("data-still", stillUrl);
+			imageTag.attr("data-animate", animateUrl);
+			imageTag.attr("data-state", "still");
+			imageTag.attr("class", "gif");
+			imageTag.attr("rating", imageRating);
+			$ ("#gifCol").prepend(imageTag);
+
+			var p = $("<p>");
+			p.text("Rated " + imageRating.toUpperCase());
+			$ ("#gifCol").prepend(p);
+
+
 		}
+		//gifLoop:==========================================
 
 
 	});
